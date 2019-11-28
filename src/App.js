210646a7040e6932957bc9fbab2dbb33c21b04ce
ouvarov <ouvarov@swivl.com>
2019-11-28 18:18:18 +0200

    get file from github

--- src/App.js
+++ src/App.js
@@ -3,14 +3,40 @@
 import React from 'react';
 import Header from './commponents/Header';
 import './App.sass';
-import './sass/style.sass'
+import './sass/style.sass';
 
-function App() {
-    return (
-        <div className="App">
-            <Header />
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            menu: {},
+        };
+    }
+    async componentDidMount() {
+        const fetchArray = await fetch(
+            `https://api.github.com/repos/ouvarov/React-Build/contents/array.json
+
+    `,
+            { method: 'GET', credentials: 'same-origin' },
+        );
+        const data = await fetchArray.json();
+        const dataDecode = decodeURIComponent(escape(window.atob(data.content)));
+        const dataJson = JSON.parse(dataDecode);
+
+        console.log(dataJson.menu);
+        // noinspection UnreachableCodeJS
+        this.setState({
+            menu: dataJson.menu,
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <div>{this.state.menu.toString()}</div>
+            </div>
+        );
+    }
 }
-
 export default App;
